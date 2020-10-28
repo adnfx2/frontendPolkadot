f6f2dec5c689eed0db79c9f7cf3439c659e0efc2
Adrian Pirela <fx.adrian@gmail.com>
2020-10-27 21:39:52 -0400

    Updates

---
+++ precache-manifest.6dfec4d128249cf71197ade6b836e9a5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87e5986a9f6dfaba18ec0cda6926d534",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5dfe0c88f01f31af040d",
+    "url": "./static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "86368f54e0ab6b61d860",
+    "url": "./static/css/main.fc54f456.chunk.css"
+  },
+  {
+    "revision": "5dfe0c88f01f31af040d",
+    "url": "./static/js/2.3be8eae5.chunk.js"
+  },
+  {
+    "revision": "8b133a7370587b662e6f1f911efa8c46",
+    "url": "./static/js/2.3be8eae5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86368f54e0ab6b61d860",
+    "url": "./static/js/main.b227139b.chunk.js"
+  },
+  {
+    "revision": "6beb490d00c1dbd63c3a",
+    "url": "./static/js/runtime-main.bcb55da4.js"
+  },
+  {
+    "revision": "8abe38181075707f30c802f97e8da92d",
+    "url": "./static/media/helloWorldPolkadot.8abe3818.png"
+  },
+  {
+    "revision": "1d10f376dde4c41f7175c896021ce367",
+    "url": "./static/media/noData.1d10f376.svg"
+  }
+]);
